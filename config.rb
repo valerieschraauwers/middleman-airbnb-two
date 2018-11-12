04a4f81a49d6e71be92b65e64c321661c2110d08
valerie schraauwers <valerieschraauwers@gmail.com>
2018-11-12 11:22:14 +0100

    added flats show page

--- config.rb
+++ config.rb
@@ -2,6 +2,10 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
+
 activate :sprockets
 
 page '/*.xml', layout: false
